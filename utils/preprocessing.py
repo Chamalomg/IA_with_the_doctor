872f4b71c45cfb11d5a079b48e4faf517ac5fd20
Fabien SOLDE <fabien.solde@epfedu.fr>
2021-01-11 16:51:00 +0100

    Add split into words

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,5 +1,14 @@
 import pandas as pd
+import re
 
 
 def process(df) -> pd.DataFrame:
     pass
+
+
+def clean_txt(str) -> str:
+    pass
+
+
+def get_words(str) -> list:
+    return re.split(r'\W+', str)
