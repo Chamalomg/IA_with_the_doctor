2768b34228e8454cd201b0d8a58828b97e5f4ae2
Fabien SOLDE <fabien.solde@epfedu.fr>
2021-01-11 13:18:05 +0100

    Select text from desired character

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import pandas as pd
 
+# Head : idx,text,type,details,episodeid,doctorid
 df = pd.read_csv("dataset/all-scripts.csv")
 
-text = 10
+text = df[df['type'] == 'talk']
 
-print(df.head())
+# Personnages étudiés
+OUR_PERS = []
+with open('dataset/personnages', 'r') as f:
+    OUR_PERS = f.readlines()
+OUR_PERS = [x.strip('\n') for x in OUR_PERS]
+
+
+# script d'un personnage en particulier
+def pers_text(df, pers) -> pd.DataFrame:
+    return text[text['details'] == pers]
+
+
+for pers in OUR_PERS:
+    print('Text from {} :'.format(pers))
+    print(pers_text(df, pers))
