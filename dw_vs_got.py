b724aaf1e652f63adca022ffb5dbd12de2eff0f5
Fabien SOLDE <fabien.solde@epfedu.fr>
2021-01-26 00:13:46 +0100

    Add who vs GoT

--- dw_vs_got.py
+++ dw_vs_got.py
@@ -1,59 +1,67 @@
 import pandas as pd
-from sklearn.metrics import confusion_matrix, classification_report
+import numpy as np
 from sklearn.utils import shuffle
+from sklearn.preprocessing import LabelBinarizer
+
 from tensorflow import keras
 from tensorflow.keras import layers
+
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from sklearn import model_selection
-import os
-import numpy as np
 
-from utils.plot import confusion
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# preprocessing utiler ça
+# https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/text/Tokenizer
+
+# votre matrice de confusion metrics
+
+from sklearn import model_selection
+
+import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path)
 
 # Const
-vocab_size = 20000
-max_length = 200
-main_character = 'DOCTOR'
+vocab_size = 10000
+max_length = 100
 
+# Extraction GOT
 # Head : idx,text,type,details,episodeid,doctorid
-df = pd.read_csv("dataset/all-scripts.csv")
-df = df.dropna()
-data = df[df['type'] == 'talk']
-data = shuffle(data)
+df_got = pd.read_csv("dataset/Game_of_Thrones_Script.csv")
+df_got = df_got.dropna()
 
-X = data['text'].tolist()
-y = data['details'].tolist()
+X_got = df_got['Sentence'].tolist()
+y_got = [1] * len(X_got)
 
-# Extract labels
-number_doctor = 0
-number_other = 0
-for i in range(len(y)):
-    if (y[i] == main_character):
-        y[i] = 1
-        number_doctor += 1
-    else:
-        y[i] = 0
-        number_other += 1
+# Extraction dw
+df_dw = pd.read_csv("dataset/all-scripts.csv")
+df_dw = df_dw.dropna()
+df_dw = df_dw[df_dw['type'] == 'talk']
 
-print("Database of " + str(len(X)) + " subtitles, with : " + str(number_doctor) + " doctor's sentences and " + str(
-    number_other) + " others's sentences : {} %".format(100 * number_doctor / (number_doctor + number_other)))
+X_dw = df_dw['text'].tolist()
+y_dw = [0] * len(X_dw)
+
+X = X_got + X_dw
+y = y_got + y_dw
 
 # split train test
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2, shuffle=True)
 
 # preprossessing tokenization + padding(to fix )
 tokenizer = Tokenizer(num_words=vocab_size, oov_token="<OOV>")
 tokenizer.fit_on_texts(X_train)
 
 X_train = tokenizer.texts_to_sequences(X_train)
-X_train = pad_sequences(X_train, maxlen=max_length, padding="post", truncating="post")
+X_train = pad_sequences(X_train, maxlen=max_length, padding="post")
 
 X_test = tokenizer.texts_to_sequences(X_test)
-X_test = pad_sequences(X_test, maxlen=max_length, padding="post", truncating="post")
+X_test = pad_sequences(X_test, maxlen=max_length, padding="post")
+
+import numpy as np
 
 X_train = np.array(X_train)
 y_train = np.array(y_train)
@@ -63,10 +71,9 @@ y_test = np.array(y_test)
 # Model
 model = keras.Sequential([
     layers.Embedding(input_dim=vocab_size, output_dim=32, input_length=max_length),
-    # layers.Conv1D(32, 7, padding="valid", activation="relu", strides=3),
     layers.GlobalAveragePooling1D(),
-    layers.Dense(32, activation='relu'),
-    layers.Dense(16, activation='relu'),
+
+    layers.Dense(64, activation='relu'),
     layers.Dense(1, activation='sigmoid')
 ])
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -74,13 +81,20 @@ model.summary()
 
 model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
 
+
+def confusion(conf):
+    sns.heatmap(conf, square=True, annot=True, cbar=False
+                , xticklabels=list(['Tyrion', 'Other'])
+                , yticklabels=list(['Tyrion', 'Other']))
+    plt.show()
+
+
 Y_pred = model.predict_generator(X_test)
 y_pred = np.argmax(Y_pred, axis=1)
 print('Confusion Matrix')
 print(confusion_matrix(y_test, y_pred))
 print('Classification Report')
-target_names = ['Other', 'Doctor']
+target_names = ['DW', 'Got']
 print(classification_report(y_test, y_pred, target_names=target_names))
 
 conf = confusion_matrix(y_test, y_pred)
-confusion()
--- main_got.py
+++ main_got.py
@@ -1,28 +1,13 @@
-import pandas as pd
-import numpy as np
 from sklearn.utils import shuffle
-from sklearn.preprocessing import LabelBinarizer
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.metrics import confusion_matrix, classification_report
-import seaborn as sns
 import pandas as pd
-
-# preprocessing utiler ça
-# https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/text/Tokenizer
-
-# votre matrice de confusion metrics
-
 from sklearn import model_selection
 import os
-import matplotlib.pylab as plt
-from matplotlib.pyplot import figure
-from tensorflow.python.keras.initializers.initializers_v2 import Constant
-from tensorflow.python.keras.utils.vis_utils import plot_model
 from sklearn.metrics import confusion_matrix
-
 from utils.glove import readGloveFile, createPretrainedEmbeddingLayer
 from utils.plot import plot_loss, confusion
 
@@ -37,12 +22,15 @@ main_character = 'tyrion lannister'
 # Head : idx,text,type,details,episodeid,doctorid
 df = pd.read_csv("dataset/Game_of_Thrones_Script.csv")
 df = df.dropna()
-print(df.head)
-data = df
-data = shuffle(data)
 
-X = data['Sentence'].tolist()
-y = data['Name'].tolist()
+df1 = df[df['Name'] == 'tyrion lannister']
+df2 = df[df['Name'] == 'jon snow']
+df = pd.concat([df1,df2])
+
+df = shuffle(df)
+
+X = df['Sentence'].tolist()
+y = df['Name'].tolist()
 
 # Extract labels
 number_main_character = 0
@@ -63,14 +51,14 @@ print("Database of " + str(len(X)) + " subtitles, with : " + str(
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
 # preprossessing tokenization + padding(to fix )
-tokenizer = Tokenizer(num_words=vocab_size, oov_token="<OOV>")
+tokenizer = Tokenizer(num_words=vocab_size)
 tokenizer.fit_on_texts(X_train)
 
 X_train = tokenizer.texts_to_sequences(X_train)
-X_train = pad_sequences(X_train, maxlen=max_length, padding="post", truncating="post")
+X_train = pad_sequences(X_train, maxlen=max_length, padding="post")
 
 X_test = tokenizer.texts_to_sequences(X_test)
-X_test = pad_sequences(X_test, maxlen=max_length, padding="post", truncating="post")
+X_test = pad_sequences(X_test, maxlen=max_length, padding="post")
 
 import numpy as np
 
@@ -84,11 +72,10 @@ def define_model(layer):
     """Create model : embedding and MLP"""
     model = keras.Sequential([
         layer,
-        # layers.Conv1D(32, 7, padding="valid", activation="relu", strides=3),
         layers.GlobalAveragePooling1D(),
         layers.Dense(32, activation='relu'),
         layers.Dense(32, activation='relu'),
-        layers.Dropout(.2),
+        layers.Dropout(.4),
         layers.Dense(1, activation='sigmoid')
     ])
     return model
@@ -124,7 +111,7 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 model.summary()
 
 history = model.fit(X_train, y_train, epochs=2, batch_size=64, validation_data=(X_test, y_test))
-print(history.history)
+# print(history.history)
 # model.save('./saved_model/lastest.keras')
 
 results = model.evaluate(X_test, y_test, batch_size=128)
@@ -141,6 +128,6 @@ conf = confusion_matrix(y_test, y_pred)
 print('Classification Report')
 target_names = ['Other', 'Tyrion']
 print(classification_report(y_test, y_pred, target_names=target_names))
-print(y_test, Y_pred)
+# print(y_test, Y_pred)
 confusion(conf)
 
