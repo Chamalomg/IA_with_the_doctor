77eae1c8d4cf25b02f11acfe34dc930145d7fdf7
Fabien SOLDE <fabien.solde@epfedu.fr>
2021-01-25 18:52:40 +0100

    Added visualisation

--- main_got.py
+++ main_got.py
@@ -12,24 +12,27 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 
-#preprocessing utiler ça 
-#https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/text/Tokenizer
+# preprocessing utiler ça
+# https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/text/Tokenizer
 
-#votre matrice de confusion metrics
+# votre matrice de confusion metrics
 
 from sklearn import model_selection
 
 import os
+import matplotlib.pylab as plt
+from matplotlib.pyplot import figure
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path)
 
-#Const
+# Const
 vocab_size = 10000
 max_length = 100
 main_character = 'tyrion lannister'
 
 # Head : idx,text,type,details,episodeid,doctorid
-df = pd.read_csv ("dataset/Game_of_Thrones_Script.csv")
+df = pd.read_csv("dataset/Game_of_Thrones_Script.csv")
 df = df.dropna()
 print(df.head)
 data = df
@@ -38,24 +41,25 @@ data = shuffle(data)
 X = data['Sentence'].tolist()
 y = data['Name'].tolist()
 
-#Extract labels
+# Extract labels
 number_main_character = 0
 number_other = 0
 for i in range(len(y)):
-    if(y[i]==main_character):
-        y[i]=1
+    if y[i] == main_character:
+        y[i] = 1
         number_main_character += 1
     else:
-        y[i]=0
+        y[i] = 0
         number_other += 1
 
-print("Database of "+str(len(X))+ " subtitles, with : "+str(number_main_character)+" main character's sentences and "+str(number_other)+ " others's sentences")
+print("Database of " + str(len(X)) + " subtitles, with : " + str(
+    number_main_character) + " main character's sentences and " + str(number_other) + " others's sentences")
 
-#split train test 
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y, test_size=0.2)
+# split train test
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
-#preprossessing tokenization + padding(to fix )
-tokenizer = Tokenizer(num_words=vocab_size ,oov_token="<OOV>")
+# preprossessing tokenization + padding(to fix )
+tokenizer = Tokenizer(num_words=vocab_size, oov_token="<OOV>")
 tokenizer.fit_on_texts(X_train)
 
 X_train = tokenizer.texts_to_sequences(X_train)
@@ -65,37 +69,38 @@ X_test = tokenizer.texts_to_sequences(X_test)
 X_test = pad_sequences(X_test, maxlen=max_length, padding="post", truncating="post")
 
 import numpy as np
+
 X_train = np.array(X_train)
 y_train = np.array(y_train)
 X_test = np.array(X_test)
 y_test = np.array(y_test)
 
-#Model
+# Model
 model = keras.Sequential([
     layers.Embedding(input_dim=vocab_size, output_dim=8, input_length=max_length),
-    #layers.Conv1D(32, 7, padding="valid", activation="relu", strides=3),
+    # layers.Conv1D(32, 7, padding="valid", activation="relu", strides=3),
     layers.GlobalAveragePooling1D(),
     layers.Dense(32, activation='relu'),
     layers.Dense(32, activation='relu'),
+    layers.Dropout(.2),
     layers.Dense(1, activation='sigmoid')
 ])
-model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
 
-model.fit(X_train, y_train, epochs=50, batch_size=20, validation_data=(X_test, y_test))
+history = model.fit(X_train, y_train, epochs=50, batch_size=20, validation_data=(X_test, y_test))
+print(history.history)
 
 
+def plot_loss(history=history):
+    figure(figsize=(8, 6))
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+plot_loss(history)
